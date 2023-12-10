a510387add6dab88e798100694a5edb32f622697
naol86 <lijnaolkasinet@gmail.com>
2023-12-10 08:55:31 +0300

    test

--- models/base_model.py
+++ models/base_model.py
@@ -20,7 +20,7 @@ class BaseModel:
         """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.update_at = self.created_at
+        self.updated_at = self.created_at
 
     def __str__(self):
         """
